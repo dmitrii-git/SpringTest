dce761b40fb2ca4d6317d04acdd92537827f78eb
Dmitrii <mikheevda1@gmail.com>
2022-01-16 14:24:29 -0400

    + Print available

--- src/main/java/Test1/AppConfig.java
+++ src/main/java/Test1/AppConfig.java
@@ -1,6 +1,5 @@
 package Test1;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan (basePackages = "Test1")
 public class AppConfig {
 
-   // public Test1.ConsoleInterface consoleInterface(Test1.ConsoleInterface consoleInterface){
-   //     Test1.ConsoleInterface console = new Test1.ConsoleInterface();
-   //     console.Start();
-   //     return consoleInterface;
     }
 
 
